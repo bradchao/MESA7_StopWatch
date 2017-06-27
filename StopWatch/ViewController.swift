798fa64b1ad7b4ab791292128553b758b0d065b7
iii <iii@userteki-MacBook.local>
2017-06-27 10:18:46 +0800

    v1

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -8,18 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var hsLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var btnStartOrStop: UIButton!
+    @IBOutlet weak var btnLapOrReset: UIButton!
+    private var isStart = false
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        return cell!
+    }
+    
+    
+    @IBAction func changeStartOrStop(_ sender: Any) {
+        isStart = !isStart
+        if isStart {
+            // Running
+            btnStartOrStop.setTitle("Stop", for: UIControlState.normal)
+            btnLapOrReset.setTitle("Lap", for: UIControlState.normal)
+            doStart()
+        }else {
+            btnStartOrStop.setTitle("Start", for: UIControlState.normal)
+            btnLapOrReset.setTitle("Reset", for: UIControlState.normal)
+            doStop()
+        }
+    }
+    
+    @IBAction func doResetOrLap(_ sender: Any) {
+        if isStart {
+            // Running ==> Lap
+            doLap()
+            
+        }else {
+            // stop ==> Reset
+            doReset()
+        }
+    }
+
+    private func doStart(){
+        
+    }
+    private func doStop(){
+        
+    }
+    private func doReset(){
+        
+    }
+    private func doLap(){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
